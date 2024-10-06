e869bfaffd3cc7980b547debd1b009404c78eeef
Priyanka Susan Sebastian <susanpriyankasebastian.com>
2024-10-06 15:53:39 +0530

    product-app

---
+++ src/components/Bar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { Link } from 'react-router-dom';
+
+const Bar = () => {
+  return (
+    <div><Box sx={{ flexGrow: 1 }}>
+    <AppBar position="fixed" sx={{backgroundColor:'#A8DF8E'}}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color:'black',fontSize: '30px' }}>
+          Easy Buy- Online Shopping App
+        </Typography>
+        <Link to={'/'}><Button sx={{ color: 'black', backgroundColor: '#FFBFBF', margin: 2, fontWeight: 20, fontSize: '18px',padding: '12px 24px' }}>Home</Button></Link>
+        <Link to={'/add'}><Button sx={{ color: 'black', backgroundColor: '#FFBFBF', margin: 2, fontWeight: 20, fontSize: '18px',padding: '12px 24px' }}>Add</Button></Link>
+      </Toolbar>
+    </AppBar>
+  </Box><br /><br /></div>
+  )
+}
+
+export default Bar
